a25f05a8073eb5c819c0e05dd5507a4419012846
Katherine Worley <kdesign216@yahoo.com>
2018-04-22 07:29:17 -0400

    requesting input from user

--- my.sh
+++ my.sh
@@ -288,3 +288,14 @@ while getopts :u:p:ab option; do
 done 
 
 echo "User: $user / Pass: $pass"
+
+
+echo "What is your name?" 
+read name
+
+echo "What is your password?"
+read -s pass
+
+read -p "What's your favorite animal? " animal
+
+echo name: $name, pass: $pass, animal: $animal 

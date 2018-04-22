0ded51dce9098c429a8a360cd05b7b941df041de
Katherine Worley <kdesign216@yahoo.com>
2018-04-22 08:00:28 -0400

    added different outputs for user

--- my.sh
+++ my.sh
@@ -300,9 +300,22 @@ done
 
 #echo name: $name, pass: $pass, animal: $animal 
 
-
+printf "\nSelect a number of the animal you'd like to have\n\n"
 select animal in "cat" "dog" "bird" "fish"
 do 
 	echo "You selected $animal!"
 	break
 done
+
+
+select option in "cat" "dog" "quit"
+do  
+	case $option in 
+		cat) echo "Cats like to sleep.";;
+		dog) echo "Dogs like to play catch.";;
+		quit) break;;
+		*) echo "I'm not sure what that is.";;
+	esac
+done
+
+

ee6af555157c9216e6f39daefaad5cd9a9fdf0c5
Katherine Worley <kdesign216@yahoo.com>
2018-04-23 05:46:13 -0400

    created a year response restriction

--- my.sh
+++ my.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # This is a basic bash script. 
-read -p "Favorite animal? " a
+read -p "Favorite animal? [cat] " a
 while [[ -z "$a" ]]; do
-	read -p "I need an answer " a 
+	a="cat" 
 done 
 echo "$a was selected." 
+
+#!/bin/bash
+# This is a basic bash script.
+read -p "What year? [nnnn] " a
+while [[ ! $a =~ [0-9]{4} ]]; do
+	read -p "A year, please! {nnnn} " a
+done
+echo "Selected year: $a" 
+
